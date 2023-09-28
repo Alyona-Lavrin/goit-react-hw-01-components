f9f8fcdf2f38e2d2758aaaae57d2dedde45b8455
Alona <polejcukalona@gmail.com>
2023-09-28 22:30:53 +0300

    stats

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,24 @@
 import css from "./Statistics.module.css"
 
-const Statistics = props => (
-<section className={css.statistics}>
-  <h2 className={css.title}>{props.title}</h2>
+const getRundomColor = () => {
+  return Math.random().toString(16).substr(-6)
+}
 
-  <ul className={css.statList}>
-    <li className={css.item}>
-      <span className={css.label}>{props.stats[0].label}</span>
-      <span className={css.percentage}>{props.stats[0].percentage}%</span>
-    </li>
-    <li className={css.item}>
-      <span className={css.label}>{props.stats[1].label}</span>
-      <span className={css.percentage}>{props.stats[1].percentage}%</span>
-    </li>
-    <li className={css.item}>
-      <span className={css.label}>{props.stats[2].label}</span>
-      <span className={css.percentage}>{props.stats[2].percentage}%</span>
-    </li>
-    <li className={css.item}>
-      <span className={css.label}>{props.stats[3].label}</span>
-      <span className={css.percentage}>{props.stats[3].percentage}%</span>
-    </li>
-  </ul>
-</section>
+const Statistics = props => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{props.title}</h2>
+
+    <ul className={css.statList}>
+      {props.stats.map((item, index) => {
+        return (
+          <li className={css.item} style={{'backgroundColor': "#" + getRundomColor()}}>
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
+          </li>
+        )
+      })}
+    </ul>
+  </section>
 );
 
 export default Statistics
