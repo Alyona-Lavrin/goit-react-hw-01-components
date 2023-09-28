ac5db3244e171968eb4b7c0b1ffd731cd0573ba0
Alona <polejcukalona@gmail.com>
2023-09-29 01:16:33 +0300

    asd

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,25 @@
 import css from "./TransactionHistory.module.css"
 
 const TransactionHistory = props => (
-    <table class="transactionHistory">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+    <table className={css.transactionHistory}>
+        <thead>
+            <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+            </tr>
+        </thead>
 
-  <tbody>
-    {props.items.map((item) => (
-        <tr>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
-        </tr>
-    ))}
-  </tbody>
-</table>
+        <tbody>
+            {props.items.map((item) => (
+                <tr>
+                    <td>{item.type}</td>
+                    <td>{item.amount}</td>
+                    <td>{item.currency}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
 );
 
 export default TransactionHistory
